f177e3ed91fe311fae2fb007be9c662246d48180
sandhiya-r3 <sandybora08@gmail.com>
2025-11-02 13:49:34 +0530

    Create booksdb.js

---
+++ booksdb.js
@@ -0,0 +1,18 @@
+let books = {
+  1: {
+    title: "The Iliad",
+    author: "Homer",
+    reviews: { user1: "Great epic!" }
+  },
+  2: {
+    title: "Things Fall Apart",
+    author: "Chinua Achebe",
+    reviews: { user2: "Powerful story!" }
+  },
+  3: {
+    title: "The Little Mermaid",
+    author: "Hans Christian Andersen",
+    reviews: { user3: "Magical and sad." }
+  }
+};
+module.exports = books;
